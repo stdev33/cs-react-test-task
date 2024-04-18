eaef886c516189419d67f5ccfdccba745d9e4f29
Serhii Tarasenko <stdev33@gmail.com>
2024-04-18 21:19:50 +0300

    save favoriteItems list using redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,40 @@ import { configureStore } from '@reduxjs/toolkit';
 import { advertsReducer } from './adverts/slice';
 import { favoritesReducer } from './favorites/slice';
 import { filterReducer } from './filter/slice';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer, persistStore } from 'redux-persist';
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist/es/constants';
+
+const rootReducer = combineReducers({
+  adverts: advertsReducer,
+  filter: filterReducer,
+  favorites: favoritesReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['favorites'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    adverts: advertsReducer,
-    filter: filterReducer,
-    favorites: favoritesReducer,
-  },
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
